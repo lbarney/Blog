13d987805ff3dbeae6ebe69aa67e236cf950f19e
Lance Barney <lbarney@littleappletech.com>
2016-06-18 09:08:51 -0600

    update not for production

--- components/blogApp.js
+++ components/blogApp.js
@@ -2,13 +2,26 @@
 var React = require('react');
 
 var Blog = React.createClass({
-	// getInitalState: function(){
-	// 	return{
-	// 		activeComponent: 'allPosts',
-	// 		activePostId: null
-	// 	}
-	// },
-	
+	getInitalState: function(){
+		return{
+			activeComponent: 'allPosts',
+			activePostId: null
+		}
+	},
+	getId: function(component, id){
+		if(component === 'editOnePost'){
+			return this.setState({activePostId: id, activeComponent: 'editPost'})
+		}else{
+			return null;
+		}
+	},
+
+	showComponent: function(){
+		if(this.state.activeComponent === 'allPosts'){
+			return <AllPostsData getId = {this.getId}/>
+		
+	},
+
 
 
 
